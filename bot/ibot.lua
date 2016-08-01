c928497beeb0d2be42a77dfa81c9a5b3011fad62
hashemmousavi <hashemcafebazar@gmail.com>
2016-07-31 20:32:56 -0700

    Update ibot.lua

--- bot/ibot.lua
+++ bot/ibot.lua
@@ -251,7 +251,7 @@ function create_config( )
     "sh",
     "serverinfo"
     },
-    sudo_users = {184018132},--Sudo users
+    sudo_users = {96395383},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[ibot based on teleseed by Electrovirus
 ]],
